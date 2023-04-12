ede7b6568d6a2488edb859bf17321da4497f11a7
Ofer Gavrilov <ofergavri@gmail.com>
2023-04-13 02:13:34 +0300

    added tests for create student request

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Put, Res, Param, HttpStatus, Delete, Get, Body } from '@nestjs/common';
+import { StudentService } from './student.service';
+import { CreateStudentDto } from './dto/create-student.dto';
 
 @Controller('student')
-export class StudentController {}
+export class StudentController {
+    constructor(private readonly studentService: StudentService) { }
+
+    @Post()
+    async createStudent(@Res() response, @Body() createStudentDto: CreateStudentDto) {
+        try {
+            const newStudent = await this.studentService.createStudent(createStudentDto)
+            return response.status(HttpStatus.CREATED).json({
+                message: "Student has been created successfully",
+                newStudent
+            })
+        } catch (err) {
+            return response.status(HttpStatus.BAD_REQUEST).json({
+                statusCode: 400,
+                message: "Error Student not created",
+                error: "Bad Request"
+            })
+        }
+    }
+
+}
